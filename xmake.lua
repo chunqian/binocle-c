9e48ea3cbcc398674b48236269f2162d5150528d
chunqian.shen <chunqian.shen@qq.com>
2021-04-19 12:01:52 +0800

    update

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ set_version("0.1.0", {build = "%Y%m%d%H%M"})
 
 set_xmakever("2.5.3")
 
-set_config("buildir", "xmake_build")
+set_config("buildir", "xmake.Build")
 set_config("mode", "debug")
 set_config("plat", "macosx")
 set_config("arch", "x86_64")
